fb1c94303d8a467f7b945c55c17c48111c3277f3
Harry_Desktop <hward.1508@gmail.com>
2023-06-11 23:50:12 +0800

    initial site done, needs responsibleness

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,58 @@
+const spanCycle = document.getElementById('spanCycle');
+const blinker = document.getElementById('blinker');
+
+const cycleValues = ["Builders.","Apprentices.","Subbies.","Sparkies.","Chippies.","Managers.","Draftsmen.","Painters.","Welders.","Brickies."];
+
+
+setInterval(function() {
+  if (blinker.style.display === "none") {
+    blinker.style.display = "inline";
+  }
+  else blinker.style.display = "none";
+},650);
+
+
+
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function displayWord() {
+  let wordString = "";
+  let cycleCounter = 8;
+  let indexCounter = 0;
+  setInterval(() => {
+    spanCycle.innerHTML = wordString;
+  },100);
+
+  function removeWord() {
+    if (wordString == "") {
+      if (cycleCounter >= 9 ) {
+        cycleCounter = 0;
+      } else cycleCounter++;
+      indexCounter = 0;
+      setTimeout(typeWord(), 250);
     } else {
-      x.style.display = "block";
+      wordString = wordString.substring(0,wordString.length-1);
+      setTimeout(removeWord,100);
     }
   }
+
+  function typeWord () {
+    if (indexCounter < cycleValues[cycleCounter].length) {
+    wordString += cycleValues[cycleCounter][indexCounter];
+    indexCounter++;
+    setTimeout(typeWord,100);
+    } else setTimeout(removeWord,1500);
+  }
+  typeWord();
+}
+
+
+displayWord()
+
